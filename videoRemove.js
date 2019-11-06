29b687b6d65346532e47d19e9b7efdaa4689b40b
Adam Shea <44814104+Adam-Shea@users.noreply.github.com>
2019-11-06 17:59:01 +0000

    Add files via upload

--- videoRemove.js
+++ videoRemove.js
@@ -2,5 +2,5 @@ var video = document.getElementById('myVideo');
 video.parentElement.removeChild(video);
 
 if (window.location == "https://portal.uspcollege.ac.uk/moodle/") {
-    document.getElementById('page-content').innerHTML = '<h1 id=PMIS class=PMISintro>You are using moodle with the Palmers Moodle Improvement Suite enabled. <br>This suite was created to help streamline the moodle experience. By using this extension, you accept that I [Adam Shea] hold no responsibility for any problems caused by it.<br>Thanks for using this software<br><a href="adam-shea.github.io">~Adam Shea</><br><br>CHANGELOG:<br></h1>';
+    document.getElementById('page-content').innerHTML = '<h1 id=PMIS class=PMISintro>You are using moodle with the Palmers Moodle Improvement Suite enabled. <br>This suite was created to help streamline the moodle experience. By using this extension, you accept that the developers of this software hold no responsibility for any problems caused by it and are not related to Palmers or Moodle in any way.<br>Thanks for using this software<br><a href="https://github.com/Adam-Shea/Palmers-Moodle-Improvement-Suite">Help contribute here</><br><br>CHANGELOG:<br></h1>';
 }
